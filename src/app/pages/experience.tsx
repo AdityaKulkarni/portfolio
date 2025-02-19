db0cf3509b0328ca9a10550c5687ac742f65df0a
Aditya Kulkarni <kulkarniaditya1997@gmail.com>
2025-02-19 11:31:41 -0800

    Added experience and projects

--- src/app/pages/experience.tsx
+++ src/app/pages/experience.tsx
@@ -1,7 +1,49 @@
+import {
+	VerticalTimeline,
+	VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import experience from "../../data/experience.json";
+import { ScreenSize, useSize } from "../hooks/useSize";
+
 const Experience = () => {
+	const deviceType = useSize();
+
 	return (
-		<div className="flex flex-row flex-1 items-center min-h-screen">
-			Experience
+		<div className="flex flex-col flex-1 items-center min-h-screen text-black py-2">
+			<div
+				className={`text-white ${
+					deviceType == ScreenSize.MOBILE
+						? "text-[40px]"
+						: "text-[120px]"
+				} text-start w-full p-4`}
+			>
+				Experience
+			</div>
+			<VerticalTimeline>
+				{experience.map((exp) => (
+					<VerticalTimelineElement
+						className="vertical-element-timeline-work"
+						date={`${exp.start} - ${exp.end}`}
+						dateClassName="text-white"
+						icon={
+							<img
+								src={exp.icon}
+								className="rounded-[50%] object-contain"
+							/>
+						}
+						iconClassName="bg-white"
+					>
+						<h3 className="vertical-timeline-element-title ">
+							{exp.title}
+						</h3>
+						<h4 className="vertical-timeline-element-subtitle ">
+							{`${exp.company}, ${exp.location}`}
+						</h4>
+						<p>{exp.description}</p>
+					</VerticalTimelineElement>
+				))}
+			</VerticalTimeline>
 		</div>
 	);
 };
--- src/app/pages/projects.tsx
+++ src/app/pages/projects.tsx
@@ -1,5 +1,43 @@
+import {
+	VerticalTimeline,
+	VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import projects from "../../data/projects.json"
+import { ScreenSize, useSize } from "../hooks/useSize";
 const Projects = () => {
-	return <div className='flex flex-row flex-1 items-center min-h-screen'>Projects</div>
-}
+	const deviceType = useSize();
+	return (
+		<div className="flex flex-col flex-1 items-center min-h-screen text-black">
+			<div className={`text-white ${deviceType == ScreenSize.MOBILE ? "text-[40px]" : "text-[120px]"} text-start w-full p-4`}>
+				Projects
+			</div>
+			<VerticalTimeline>
+				{projects.map((project) => (
+					<VerticalTimelineElement
+						className="vertical-element-timeline-work"
+						date={`${project.start} - ${project.end}`}
+						dateClassName="text-white"
+						icon={
+							<img
+								src={project.icon}
+								className="rounded-[50%] object-contain"
+							/>
+						}
+						iconClassName="bg-white"
+					>
+						<h3 className="vertical-timeline-element-title ">
+							{project.company}
+						</h3>
+						<h4 className="vertical-timeline-element-subtitle ">
+							{`${project.title}, ${project.location}`}
+						</h4>
+						<p>{project.description}</p>
+					</VerticalTimelineElement>
+				))}
+			</VerticalTimeline>
+		</div>
+	);
+};
 
-export default Projects
+export default Projects;
