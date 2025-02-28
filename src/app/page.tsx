4cc8c55ea42bde8fbfccb095f0b6f76cf4eb8a84
Aditya Kulkarni <kulkarniaditya1997@gmail.com>
2025-02-27 20:42:54 -0800

    Responsive changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,11 @@ import Projects from "./pages/projects";
 import Medium from "./pages/medium";
 import Hobbies from "./pages/hobbies";
 import Skills from "./pages/skills";
+import { useEffect } from "react";
 
 export default function Home() {
 	return (
-		<main className="flex flex-col bg-black text-white lg:px-[20%]">
+		<main className="flex flex-col bg-black text-white 2xl:px-[20%]">
 			<Introduction />
 			<Experience />
 			<Projects />
